1afbabae7eb2aee9ecebf08c43ce43c47d2c5967
Christian Ribas <ribaschristian@hotmail.com>
2025-08-02 18:02:13 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -22,3 +22,19 @@ def filtrar_lista_por_numeros_primos(lista):
     lista_filtrada = []
     # Completar funcao aqui!
     return lista_filtrada
+    
+
+def verificar_se_eh_primo(numero):
+    if numero > 1:
+        for i in range(2, int(numero / 2) + 1):
+            if (numero % i) == 0:
+                return False
+        return True
+    return False
+
+def filtrar_lista_por_numeros_primos(lista):
+    lista_filtrada = []
+    for numero in lista:
+        if verificar_se_eh_primo(numero):
+            lista_filtrada.append(numero)
+    return lista_filtrada
